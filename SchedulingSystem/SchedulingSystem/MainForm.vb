031508da3303781ce7e22cddceb512a2d8a6b044
hao guang kok <haoguang18@hotmail.com>
2017-03-28 00:05:24 +0800

    Added Schedule Side Panel Button for own module

--- SchedulingSystem/SchedulingSystem/MainForm.vb
+++ SchedulingSystem/SchedulingSystem/MainForm.vb
@@ -1,13 +1,16 @@
 ﻿Public Class MainForm
     Private Sub btnSchedule_Click(sender As Object, e As EventArgs) Handles btnSchedule.Click
         Dim UserCtrl1 As New TestUserControl1
-        SidePanel.Controls.Clear()
-        SidePanel.Controls.Add(UserCtrl1)
+        Dim SideContent As New ScheduleSidePanel
+        ContentPanel.Controls.Clear()
+        ContentPanel.Controls.Add(UserCtrl1)
+        SideContentPanel.Controls.Clear()
+        SideContentPanel.Controls.Add(SideContent)
     End Sub
 
     Private Sub btnAppointment_Click(sender As Object, e As EventArgs) Handles btnAppointment.Click
         Dim UserCtrl2 As New SecondUserControl
-        SidePanel.Controls.Clear()
-        SidePanel.Controls.Add(UserCtrl2)
+        ContentPanel.Controls.Clear()
+        ContentPanel.Controls.Add(UserCtrl2)
     End Sub
 End Class
---
+++ SchedulingSystem/SchedulingSystem/Schedule/ScheduleSidePanel.vb
@@ -0,0 +1,3 @@
+﻿Public Class ScheduleSidePanel
+
+End Class
