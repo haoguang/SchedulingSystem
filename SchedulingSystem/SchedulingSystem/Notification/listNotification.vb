983670553e23ade7002b459734e5d145327157d7
KYT <ktkarensakura255@gmail.com>
2017-04-14 21:35:59 +0800

    Notification

---
+++ SchedulingSystem/SchedulingSystem/Notification/listNotification.vb
@@ -0,0 +1,27 @@
+﻿Public Class listNotification
+    Public index As Integer = 0
+    Dim userId As Integer = LoginSession.memberID
+
+    Private Sub listNotification_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim db As New ScheduleDBDataContext()
+
+        Dim friendNotification = From f In db.Friends, m In db.Members
+                                 Where f.FriendID = userId And f.Status = "pending" And f.UserID = m.MemberID
+                                 Select New With {
+                                     m.Username
+                                   }
+
+        Dim appointmentNotification = From p In db.Participles, m In db.Members, s In db.Schedules
+                                      Where p.MemberID = userId And p.Status = "pending" And p.ScheduleID = s.ScheduleID
+                                      Select New With {
+                                           s.Title,
+                                           s.ScheduleTimes
+                                   }
+    End Sub
+
+    Private Sub lstNotification_MouseDoubleClick(sender As Object, e As EventArgs) Handles lstNotification.MouseDoubleClick
+        index = lstNotification.SelectedIndex
+        'showNotification.ShowDialog()
+    End Sub
+End Class
