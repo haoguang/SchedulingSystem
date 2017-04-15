8d1dc3d9736602b1859903029501513c02d5a97d
KYT <ktkarensakura255@gmail.com>
2017-04-15 08:58:57 +0800

    Notiication

--- SchedulingSystem/SchedulingSystem/Notification/listNotification.vb
+++ SchedulingSystem/SchedulingSystem/Notification/listNotification.vb
@@ -5,33 +5,41 @@
     Private Sub listNotification_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
         Dim db As New ScheduleDBDataContext()
+        Dim currentDateTime As DateTime
 
-        Dim friendNotification = From f In db.Friends, m In db.Members
-                                 Where f.UserID = userId And f.Status = "pending" And f.Inviter = m.MemberID
-                                 Select New With {
-                                     m.Username
-                                   }
+        currentDateTime = DateTime.Now
 
-        Dim appointmentNotification = From p In db.Participles, m In db.Members, s In db.Schedules
-                                      Where p.MemberID = userId And p.Status = "pending" And p.ScheduleID = s.ScheduleID
-                                      Select New With {
-                                           s.Title,
-                                           s.ScheduleTimes
-                                   }
+        Dim friendQuery = From f In db.Friends, m In db.Members
+                          Where f.UserID = userId And f.Status = "pending" And f.Inviter = m.MemberID
+                          Select m
 
-        If (friendNotification IsNot Nothing) Then
-            lstNotification.Items.Add(friendNotification)
-        ElseIf (appointmentNotification IsNot Nothing) Then
-            lstNotification.Items.Add(appointmentNotification)
-        ElseIf (friendNotification Is Nothing And appointmentNotification Is Nothing) Then
-            lstNotification.Items.Add("N/A")
-        ElseIf (friendNotification IsNot Nothing And appointmentNotification IsNot Nothing) Then
-            lstNotification.Items.Add("Hello")
+        Dim appointmentQuery2 = From p In db.Participles, m In db.Members, s In db.Schedules, st In db.ScheduleTimes
+                                Where p.MemberID = userId And p.Status = "pending" And p.ScheduleID = s.ScheduleID And s.ScheduleID = st.ScheduleID
+                                Select s.ScheduleID, st.ScheduleStart, s.Title
+
+        'if exist
+        If appointmentQuery2.Count > 0 Then
+            For Each s In appointmentQuery2
+                lstNotification.Items.Add(String.Format("There is a {0} request on {1}.  {2}  {3}", s.Title, s.ScheduleStart, s.ScheduleID, appointmentQuery2.Count))
+            Next
+        Else
+            lstNotification.Items.Add("Appointment N/A")
         End If
+
+        If friendQuery.Count > 0 Then
+            For Each m In friendQuery
+                lstNotification.Items.Add(String.Format("{0}   {1}", m.Username, friendQuery.Count))
+            Next
+        Else
+            lstNotification.Items.Add("friend N/A")
+        End If
+
+
     End Sub
 
     Private Sub lstNotification_MouseDoubleClick(sender As Object, e As EventArgs) Handles lstNotification.MouseDoubleClick
         index = lstNotification.SelectedIndex
-        'showNotification.ShowDialog()
+        FriendNotification.ShowDialog()
+
     End Sub
 End Class
